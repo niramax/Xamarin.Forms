a92005d3c8c52f7d427e45eca8a1e16326468b6b
Frank A. Krueger <fak@praeclarum.org>
2023-07-07 12:01:57 -0700

    Suppress deprecation warnings

---
+++ Xamarin.Forms.Platform.iOS/GlobalSuppressions.cs
@@ -0,0 +1,9 @@
+// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Code Notifications", "CA1416", Justification = "Forms is built on deprecated APIs")]
+[assembly: SuppressMessage("Code Notifications", "CA1422", Justification = "Forms is built on deprecated APIs")]
